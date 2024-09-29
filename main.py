fa144f0ee2cf9c213f4c7eb8025d5b8293ca9351
Pro696969 <krishnakumaarmc@gmail.com>
2024-09-29 12:03:18 +0530

    working1

--- main.py
+++ main.py
@@ -33,16 +33,19 @@ select = Select(driver.find_element(By.ID, 'semesters'))
 select.select_by_value('2518') #sem5
 # #map these valuse to a dictionary to generalize later on
 time.sleep(2)
-SE = driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/div/div/div[2]/div/div[3]/div/div/table/tbody/tr[1]')
+SE = driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/div/div/div[2]/div/div[3]/div/div/table/tbody/tr[1]/td[1]')
+SE.click()
 
-# rowWiseCourseContent_19053 = driver.find_element(By.ID, 'rowWiseCourseContent_19053').click()
-row = driver.find_element(By.XPATH, "//tr[@id='rowWiseCourseContent_19053']")
-ele = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//tr[@id='rowWiseCourseContent_19053']")))
-ele.click()
+
+# GOD FKING KNOWS HOW SELENIUM WORKS SHATA 
+# row = driver.find_element(By.XPATH, "//tr[@id='rowWiseCourseContent_19053']")
+# time.sleep(1)
+# ele = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, '/html/body/div[4]/div[2]/div/div/div[2]/div/div[3]/div/div/table/tbody/tr[1]/td[2]')))
+# driver.execute_script("arguments[0].scrollIntoView(true);", ele)
+# ele.click()
 # row.click()
 # print(SE)
 
 
 
-time.sleep(200)
 driver.close()
